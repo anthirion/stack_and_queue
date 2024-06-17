c3f10c3fd1335044444a7712d218025976efaee9
Antoine Thirion <antoth.web@outlook.fr>
2024-06-17 20:49:35 +0200

    [fix] erreur dans la pile lors d'un array plus grand que la taille par défaut

--- src/main.c
+++ src/main.c
@@ -9,15 +9,15 @@ int main() {
     /*
     * Test stack operations
     */
-    int elements_array[6] = {1, 4, 9, 12, -5, -8};
+    // int elements_array[6] = {1, 4, 9, 12, -5, -8};
     // array that has more element that the default size
-    // int elements_array[12] = {1, 4, 9, 12, -5, -8, 3, 98, 22, 44, 42, 65};
+    int elements_array[12] = {1, 4, 9, 12, -5, -8, 3, 98, 22, 44, 42, 65};
 
-    stack st = create_stack(elements_array, 6);
-    // push(&st, 6);
+    stack st = create_stack(elements_array, 12);
+    push(&st, 8);
     printf("The element at the top of the stack is: %d\n", top(st));
     pop(&st);
-    pop(&st);
+    // pop(&st);
     printf("The element at the top of the stack is: %d\n", top(st));
     printf("The list is empty: %s\n", isEmptyStack(st) ? "true" : "false");
     printf("The list has %d elements\n\n", get_stack_length(st));
@@ -26,13 +26,13 @@ int main() {
     /*
     * Test queue operations
     */
-   queue q = create_queue(elements_array, 6);
-   enqueue(&q, 5);
-   dequeue(&q);
-   dequeue(&q);
-   printf("The next element of the queue is: %d\n", first(q));
-   printf("The last queued element is: %d\n", last(q));
-   freeQueue(&q);
+//    queue q = create_queue(elements_array, 12);
+//    enqueue(&q, 5);
+//    dequeue(&q);
+//    dequeue(&q);
+//    printf("The next element of the queue is: %d\n", first(q));
+//    printf("The last queued element is: %d\n", last(q));
+//    freeQueue(&q);
 
     return 0;
 }
--- src/stack.c
+++ src/stack.c
@@ -21,7 +21,7 @@ void push(stack* pst, int element) {
             for (int i=0; i < pst->max_length/2; i++)
                 new_stack_ptr[i] = pst->stack_ptr[i];
             // Free the old stack
-            freeStack(pst);
+            free(pst->stack_ptr);
             // Update the stack pointer
             pst->stack_ptr = new_stack_ptr;
         }
@@ -32,7 +32,7 @@ void push(stack* pst, int element) {
         }
     }
     // Add the element at the end
-    pst->stack_ptr[pst->length] = element;
+    pst->stack_ptr[pst->length - 1] = element;
 }
 
 stack create_stack(const int* elements_array, const int length) {
@@ -52,7 +52,7 @@ void pop(stack* pst) {
 }
 
 int top(const stack st) {
-    return st.stack_ptr[st.length];
+    return st.stack_ptr[st.length - 1];
 }
 
 bool isEmptyStack(const stack st) {
